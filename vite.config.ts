d7a240c1dadad888803a4a0a815b93743f988835
Shouri Chakraborty <107758587+shouri123@users.noreply.github.com>
2026-04-29 11:47:33 +0530

    Add Vite and TypeScript build system

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import { crx } from '@crxjs/vite-plugin';
+import manifest from './src/manifest.json';
+
+export default defineConfig({
+  base: './',
+  plugins: [crx({ manifest })],
+  build: {
+    rollupOptions: {
+      input: {
+        dashboard: 'src/dashboard.html',
+        options: 'src/options.html',
+        popup: 'src/popup.html',
+        offscreen: 'src/offscreen.html'
+      }
+    }
+  }
+});
